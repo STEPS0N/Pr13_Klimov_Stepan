42bc770749fc11619dcdbff94f3398b79625bb06
Степан Климов <stepan.chik228@mail.ru>
2025-10-24 19:22:41 +0500

    Реализованные пользовательские элементы управления. Климов

---
+++ Interface_Klimov/Elements/Messages.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Interface_Klimov.Classes;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Interface_Klimov.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Messages.xaml
+    /// </summary>
+    public partial class Messages : UserControl
+    {
+        public MessagesContext ThisMessage;
+        
+        public Messages(Classes.MessagesContext messages)
+        {
+            InitializeComponent();
+            ThisMessage = messages;
+            Message.Text = messages.Message;
+            Date.Text = messages.Create.ToString("dd.MM.yyyy");
+        }
+
+        private void DeleteMessage(object sender, MouseButtonEventArgs e)
+        {
+            ThisMessage.Delete();
+            MainWindow.mainWindow.parentMessage.Children.Remove(this);
+        }
+    }
+}
---
+++ Interface_Klimov/Elements/Users.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Interface_Klimov.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Users.xaml
+    /// </summary>
+    public partial class Users : UserControl
+    {
+        public Models.Users ThisUser;
+        
+        public Users(Models.Users User)
+        {
+            InitializeComponent();
+            ThisUser = User;
+            FIO.Text = User.FIO;
+        }
+
+        private void SelectUser(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.SelectUser(ThisUser);
+        }
+    }
+}
