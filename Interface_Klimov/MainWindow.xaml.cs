0b4995652a4efbe4fb96ba19d40a20cf070645c6
Степан Климов <stepan.chik228@mail.ru>
2025-10-24 19:44:52 +0500

    Реализованная практическая работа на оценку удовлетворительно. Климов

--- Interface_Klimov/MainWindow.xaml.cs
+++ Interface_Klimov/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using Interface_Klimov.Classes;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Interface_Klimov
 {
@@ -16,9 +9,50 @@ namespace Interface_Klimov
     /// </summary>
     public partial class MainWindow : Window
     {
+        public UsersContext usersContext = new Classes.UsersContext();
+        public MessagesContext messagesContext = new Classes.MessagesContext();
+        public int IdSelectUser = -1;
+        public static MainWindow mainWindow;
+        
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
+            LoadUser();
+        }
+
+        private void SendMessages(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (IdSelectUser == -1) return;
+                MessagesContext newMessages = new MessagesContext(Message.Text, DateTime.Now, IdSelectUser);
+                newMessages.Save();
+                Message.Text = "";
+                SelectUser(null);
+            }
+        }
+
+        public void LoadUser()
+        {
+            foreach (Models.Users User in usersContext.AllUsers)
+            {
+                ParentUser.Children.Add(new Elements.Users(User));
+            }
+        }
+
+        public void SelectUser(Models.Users User)
+        {
+            if (User != null)
+            {
+                IdSelectUser = usersContext.AllUsers.FindIndex(x => x == User);
+            }
+            parentMessage.Children.Clear();
+            foreach (MessagesContext Messages in MessagesContext.AllMessages.FindAll(x => x.IdUser == IdSelectUser))
+            {
+                parentMessage.Children.Add(new Elements.Messages(Messages));
+            }
+            BlockMessage.IsEnabled = true;
         }
     }
 }
