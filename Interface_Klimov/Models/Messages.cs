0309a7cf27a40c18827cbab2e11265bbfb07dbe3
Степан Климов <stepan.chik228@mail.ru>
2025-10-24 18:38:24 +0500

    Реализованные модели. Климов

---
+++ Interface_Klimov/Models/Messages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Klimov.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Create { get; set; }
+        public int IdUser { get; set; }
+
+        public Messages() { }
+
+        public Messages(string message, DateTime create, int idUser)
+        {
+            this.Message = message;
+            this.Create = create;
+            this.IdUser = idUser;
+        }
+    }
+}
---
+++ Interface_Klimov/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Klimov.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+
+        public Users() { }
+
+        public Users(int id, string fIO)
+        {
+            this.Id = id;
+            this.FIO = fIO;
+        }
+    }
+}
